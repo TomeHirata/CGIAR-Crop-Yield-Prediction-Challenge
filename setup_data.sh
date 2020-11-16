b70fc6855a93faf1598c14630dc34675f9aea668
TomeHirata <tomu.hirata@gmail.com>
2020-11-17 00:41:38 +0900

    fix setup.sh

--- setup_data.sh
+++ setup_data.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-FILE_ID=1OD7cINKbpKPmrjqV7ohrSGujMNlmy-tf
+mkdir data
+FILE_ID='12VANjv1bf1Alj2utOmVuOw5dUuqVwsOo'
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o data
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o data/image_arrays_test.zip
+FILE_ID='1Bku37-ZlhYJBRYklR5vlO8ET0bCulrtV'
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o data/image_arrays_train.zip
 echo "Download Success!!!"
 unzip -q data/image_arrays_train.zip
 unzip -q data/image_arrays_test.zip
